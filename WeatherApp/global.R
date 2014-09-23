1be7bf00cb889b7cd21c839ba4efd77300b63e65
Greg Doherty <greg@dohertys.org>
2014-09-22 23:30:41 -0700

    try to make faster with setkey()

--- WeatherApp/global.R
+++ WeatherApp/global.R
@@ -1,7 +1,10 @@
 # per application stuff
 library(dplyr,warn.conflicts = F,quietly=T)
+library(data.table)
 
 load("data/ALL.dat")
+
+setkey(meas,id,time,elem)
 
 bdate <- "2013-01-01"
 edate <- "2014-09-30"
